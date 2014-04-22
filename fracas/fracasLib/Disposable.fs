883b3f408fa3ebbca8471a6079efee4642e873e6
Curt Nichols <curt.nichols@live.com>
2014-04-21 21:20:00 -0700

    Added module Disposable

---
+++ fracas/fracasLib/Disposable.fs
@@ -0,0 +1,13 @@
+﻿module Disposable
+
+open System
+
+/// Invokes 'first', then disposes of the disposables, afterward setting disposed to true.
+let disposeWith (disposed: bool ref) (disposables: IDisposable list) (first: unit -> unit) =
+    first()
+    disposables |> List.iter (fun d -> d.Dispose())
+    disposed := true
+
+/// Disposes of the disposables, afterward setting disposed to true.
+let dispose (disposed: bool ref) (disposables: IDisposable list) =
+    (fun () -> ()) |> disposeWith disposed disposables
