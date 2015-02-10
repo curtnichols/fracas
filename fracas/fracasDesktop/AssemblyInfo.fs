5b4b82871830d3430f917435e1d872e034367dbd
Curt NIchols <cnichols@soundmetrics.com>
2015-02-10 09:51:20 -0800

    Add AssemblyInfo.fs

---
+++ fracas/fracasDesktop/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+﻿module AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("fracas")>]
+[<assembly: AssemblyDescription("Minimal MVVM Support Library")>]
+[<assembly: AssemblyConfiguration("Desktop/WPF")>]
+[<assembly: AssemblyCompany("Curt Nichols")>]
+[<assembly: AssemblyProduct("fracas")>]
+[<assembly: AssemblyCopyright("Copyright © Curt Nichols 2014-2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+do ()
